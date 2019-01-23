f0ec973e9bdff760fa67e5fefde6259e245f1a8f
Jeffrey Varner <jdv27@cornell.edu>
2019-01-23 15:09:18 -0500

    Working with package documentation

--- docs/index.md
+++ docs/index.md
@@ -5,11 +5,14 @@ We use this model as a teaching tool in [CHEME 7770 Advanced Biomolecular Engine
 
 ### Requirements and Installation
 To use this model, you need a couple of things. First, [Julia](https://julialang.org) version 1.0 or greater must be installed on your local machine.
-Alternatively, you can use [JuliaBox](https://juliabox.com) in the cloud. Next, you'll need the model code. You can download the code (either as a zip) or by cloning from GitHub:
+Alternatively, you can use [JuliaBox](https://juliabox.com) in the cloud. Next, you'll need the model code. You can download the code either as a zip file, or by cloning directly from GitHub:
 
     git clone https://github.com/varnerlab/Core_Escherichia_coli_model.git
 
-Once the model code is own your machine (or on your [JuliaBox](https://juliabox.com) instance), to execute model calculations, start the [Julia](https://julialang.org) REPL in the ``src`` subdirectory and
-execute the command:
+Once the model code is own your machine (or on your [JuliaBox](https://juliabox.com) instance in the cloud), to execute model calculations, start the [Julia](https://julialang.org) REPL in the ``src`` subdirectory and
+execute a sample command:
 
-    julia> include("Solve.jl")
+    julia> include("Test.jl")
+
+#### What does Test.jl do?
+``Test.jl`` downloads (and installs) required packages that are not already installed on your machine. It then maximizes the aerobic growth of _E. coli_ on glucose with some example external measurement distrubutions.
---
+++ src/Test.jl
@@ -0,0 +1,114 @@
+# includes -
+include("Include.jl")
+
+# setup some global consts -
+const path_to_cobra_mat_file = "$(pwd())/cobra/config/matlab_cobra_files/modelReg.mat"
+const model_file_name = "modelReg"
+const organism_id = :eco    # we use the KEGG organism symbols
+const path_to_measurements_file = "$(pwd())/experimental_data/test_data/Glucose.json"
+
+# declare a return type for the flux variability calculation -
+mutable struct VLFluxVariabilityResult
+
+    optimal_flux_array::Array{Float64,1}
+    flux_variability_array::Array{Float64,1}
+
+    # constructor -
+    function VLFluxVariabilityResult()
+        this = new()
+    end
+end
+
+# declare a return type -
+mutable struct VLOptimalFluxResult
+
+    objective_value::Float64
+    flux_array::Array{Float64,1}
+    dual_array::Array{Float64,1}
+    uptake_array::Array{Float64,1}
+    exit_flag::Int64
+    status_flag::Int64
+    flux_bounds_array::Array{Float64,2}
+
+    # constructor -
+    function VLOptimalFluxResult()
+        this = new()
+    end
+end
+
+
+"""
+TODO: Fill me in with some stuff ...
+"""
+function maximize_specific_growth_rate(path_to_measurements_file::String; number_of_samples::Int64 = 100)
+
+    # initalize -
+    results_array = Array{VLOptimalFluxResult,1}()
+
+    # Declare a progress meter for user feedback -
+    p = Progress(number_of_samples,color=:yellow)
+
+    # ok, so lets sample ...
+    for sample_index = 1:number_of_samples
+
+        # load the default data_dictionary -
+        default_data_dictionary = generate_default_data_dictionary(path_to_cobra_mat_file, model_file_name, organism_id);
+
+        # pass the default dictionary to a customization method -
+        updated_data_dictionary = optimize_specific_growth_rate(default_data_dictionary);
+
+        # update dictionary with experimental data?
+        updated_data_dictionary = constrain_measured_fluxes(updated_data_dictionary, path_to_measurements_file);
+
+        # estimate the optimal flux distrubution -
+        (objective_value, calculated_flux_array, dual_value_array, uptake_array, exit_flag, status_flag) = calculate_optimal_flux_distribution(updated_data_dictionary);
+
+        # build a return type -
+        fluxResult = VLOptimalFluxResult();
+        fluxResult.objective_value = objective_value;
+        fluxResult.flux_array = calculated_flux_array;
+        fluxResult.dual_array = dual_value_array;
+        fluxResult.uptake_array = uptake_array;
+        fluxResult.exit_flag = exit_flag;
+        fluxResult.status_flag = status_flag;
+
+        # get some problem setup information -
+        fluxResult.flux_bounds_array = updated_data_dictionary["flux_bounds_array"];
+
+        # cache -
+        push!(results_array, fluxResult);
+
+        # user message -
+        msg = "Completed $(sample_index) of $(number_of_samples) trials ...";
+
+        # update the progress bar -
+        ProgressMeter.next!(p; showvalues = [(:status,msg)]);
+    end
+
+    @info "Completed ...\r";
+
+    # how many flux are there?
+    number_of_fluxes = length(results_array[1].flux_array);
+
+    # compute the flux values -
+    flux_ensemble = zeros(number_of_fluxes,1);
+    for flux_object in results_array
+
+        # grab the flux -
+        flux_array = flux_object.flux_array;
+
+        # cache -
+        flux_ensemble = [flux_ensemble flux_array];
+    end
+
+    # cut off the zeros -
+    flux_ensemble = flux_ensemble[:,2:end];
+
+    # compute the mean and std -
+    µ = mean(flux_ensemble, dims=2);
+    σ = std(flux_ensemble, dims=2);
+    flux_distribution = [µ σ];
+
+    # return -
+    return (flux_distribution, results_array)
+end
